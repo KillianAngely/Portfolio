2d81b1e81de0cf507026626c2403d66837e709d7
killian <killian@MacBook-Pro-de-killian.local>
2023-11-25 13:27:38 +0100

    init scrapper and githubApi fetch

---
+++ Data/Scrapper/exctract_linkedin_data.py
@@ -0,0 +1,85 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeType
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+class Driver:
+    def __init__(self):
+        self.options = Options()
+        self.options.add_argument("--headless")
+        self.options.add_argument("--start-maximized")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_experimental_option("detach", True)
+        self.options.add_argument("--disable-dev-shm-usage")
+        user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"
+        self.options.add_argument(f"user-agent={user_agent}")
+        self.options.add_argument("--window-size=1920,1080")
+        self.chrome_service = webdriver.ChromeService(
+            driver_version="118.0.5993.70", chrome_type=ChromeType.GOOGLE
+        )
+        self.driver = webdriver.Chrome(service=self.chrome_service, options=self.options)
+
+    def get_driver(self):
+        return self.driver
+    
+class LinkedinScraper:
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def login(self, username, password):
+        self.driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
+        WebDriverWait(self.driver, 10).until(
+            lambda d: d.find_element(By.CSS_SELECTOR, ".login__form")
+            )
+        login_form = self.driver.find_element(By.CSS_SELECTOR, ".login__form")
+        if login_form is not None:
+            print("Login page found")
+            #find in login form
+            username_input = login_form.find_element(By.CSS_SELECTOR, "#username")
+            password_input = login_form.find_element(By.CSS_SELECTOR, "#password")
+            if username_input is not None and password_input is not None:
+                print("Username and password found")
+                username_input.send_keys(username)
+                password_input.send_keys(password)
+                login_button = login_form.find_element(By.CSS_SELECTOR, ".btn__primary--large.from__button--floating")
+                if login_button is not None:
+                    print("Login button found")
+                    login_button.click()
+                else:
+                    print("Login button not found")
+
+    def extract_data_profile(self, profile_url):
+        self.driver.get(profile_url)
+        WebDriverWait(self.driver, 10).until(
+            lambda d: d.find_element(By.CSS_SELECTOR, "#ember32 button #ember37")
+            )
+        main_page = self.driver.find_element(By.CSS_SELECTOR, ".scaffold-layout__inner.scaffold-layout-container.scaffold-layout-container--reflow")
+        if main_page is not None:
+            print("page found")
+            self.get_experience()
+
+    def get_experience(self):
+        WebDriverWait(self.driver, 10).until(
+            lambda d: d.find_element(By.CSS_SELECTOR, ".pv-profile-section.experience-section.ember-view")
+            )
+        pass
+            
+
+
+def main():
+    driver = Driver().get_driver()
+    linkedin_scraper = LinkedinScraper(driver)
+    username = os.getenv("LINKEDIN_USERNAME")
+    password = os.getenv("LINKEDIN_PASSWORD")
+    linkedin_scraper.login(username,password)
+    linkedin_scraper.extract_data_profile("https://www.linkedin.com/in/killian-angely/")
+    
+
+if __name__ == "__main__":
+    main()
